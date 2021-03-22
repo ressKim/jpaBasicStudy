c903a960d7a999c999e1f8e463577f8233f86955
ressKim <67080787+ressKim@users.noreply.github.com>
2021-03-23 08:11:48 +0900

    jpa-basic-study start

---
+++ src/main/java/basicjpa/studyjpabasic/StudyJpaBasicApplication.java
@@ -0,0 +1,13 @@
+package basicjpa.studyjpabasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyJpaBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyJpaBasicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/basicjpa/studyjpabasic/StudyJpaBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package basicjpa.studyjpabasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyJpaBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
