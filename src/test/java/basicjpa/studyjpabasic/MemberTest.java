f558bb753c9062d956c6507750c14f953d67a429
ressKim <67080787+ressKim@users.noreply.github.com>
2021-03-29 07:53:43 +0900

    id 생성전략 study

--- src/test/java/basicjpa/studyjpabasic/MemberTest.java
+++ src/test/java/basicjpa/studyjpabasic/MemberTest.java
@@ -3,6 +3,7 @@ package basicjpa.studyjpabasic;
 import basicjpa.studyjpabasic.domain.Member;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,14 +17,12 @@ class MemberTest {
 //    EntityManagerFactory emf = Persistence.createEntityManagerFactory();
 //
 //    EntityManager em = emf.createEntityManager();
-     EntityManager em;
 
-
-
-
+    @Autowired
+    EntityManager em;
 
     @Test
-    public void memberTest(){
+    public void memberTest() {
         Member setMember = new Member();
         setMember.setId(101L);
         setMember.setName("DDDDD");
