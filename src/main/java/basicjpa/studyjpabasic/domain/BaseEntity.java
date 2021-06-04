a95b6c0c6542a0f4ba08e810a07f25d6df906e54
ressKim <sh21dlat@gmail.com>
2021-06-04 21:41:45 +0900

    BaseEntity 수정

---
+++ src/main/java/basicjpa/studyjpabasic/domain/BaseEntity.java
@@ -0,0 +1,19 @@
+package basicjpa.studyjpabasic.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Getter
+@Setter
+public abstract class BaseEntity {
+
+    private String createdBy;
+    private LocalDateTime createdDate;
+    private String lastModifiedBy;
+    private LocalDateTime lastModifiedDate;
+}
